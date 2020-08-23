dce55f243b17b2c90e1684ba3da6de4995f91a6d
Klairm <ivangardis@hotmail.com>
2020-08-23 17:18:49 +0200

    Optimized 2 functions in 1

--- exporters.py
+++ exporters.py
@@ -3,24 +3,10 @@ from time import sleep
 from pydub import AudioSegment
 from progress.spinner import PixelSpinner
 
-def mp3_ogg(name,ext,path,folder):
+def converter(name,ext,path,form):
 	with PixelSpinner(f'{name} Processing... ') as bar:
 		for i in range(100):
-			sleep(0.06)
+			sleep(0.03)
 			bar.next()
-		sound = AudioSegment.from_ogg(name+ext)
-		if folder == 'export':
-			sound.export(f"{path}/{name}.ogg", format="ogg")
-		else:
-			sound.export(f"{path}/export/{name}.ogg", format="ogg")
-
-def ogg_mp3(name,ext,path,folder):
-	with PixelSpinner(f'{name} Processing... ') as bar:
-		for i in range(100):
-			sleep(0.06)
-			bar.next()
-		sound = AudioSegment.from_ogg(name+ext)
-		if folder == 'export':
-			sound.export(f"{path}/{name}.mp3", format="mp3")
-		else:
-			sound.export(f"{path}/export/{name}.mp3", format="mp3")
+		sound = AudioSegment.from_mp3(name+ext)
+		sound.export(f"{path}/export/{name}.{form}", format=form)
